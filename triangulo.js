3c309803535ef1e4ce28d3e8d58197bba7701320
Fernanda Ávila <153337144+fernanda-avila@users.noreply.github.com>
2024-09-11 20:01:21 -0300

    Update triangulo.js

--- triangulo.js
+++ triangulo.js
@@ -1,17 +1,17 @@
-const prompt = require('prompt-sync')();
-let linhas = parseInt(prompt("Digite o número de linhas do triângulo: "));
+const prompt = require("prompt-sync")();
+
+let linhas = parseInt(prompt("Digite o número de linhas do triângulo:"));
 
 for (let i = 1; i <= linhas; i++) {
     let linha = '';
 
-    for (let j = 1; j <= i; j++) {
+    for (let j = 1; j <= (linhas - i); j++) {
         linha += ' ';
     }
 
-    for (let j = 1; j <= (linhas - i) * 2 + 1; j++) {
+    for (let k = 1; k <= (2 * i - 1); k++) {
         linha += '*';
     }
 
     console.log(linha);
 }
-
