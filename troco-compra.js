9f56bbdeb2245d64d16d35266a0e3e60affe9bc4
Fernanda Ávila <153337144+fernanda-avila@users.noreply.github.com>
2024-09-23 09:56:29 -0300

    Atualizar o troco-compra.js

--- troco-compra.js
+++ troco-compra.js
@@ -1,6 +1,6 @@
 //Este programa solicita ao usuário que insira o valor da compra e o valor pago pelo cliente,
 //calcula o troco e exibe o resultado em uma mensagem de alerta.
 const valorCompra = Number(prompt("Digite o valor da compra:"));
-const valorPago = Number(prompt("Digite o valor pado pelo cliente"));
+const valorPago = Number(prompt("Digite o valor pago pelo cliente"));
 const troco = valorPago - valorCompra;
 alert(`O troco é de R$ ${troco.toFixed(2)}`);
